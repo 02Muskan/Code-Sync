0a2b2f85be57cb230e566ae9649fe478bd1b0111
Muskan Agarwal <agarwalmuskan052@gmail.com>
2025-07-08 22:02:01 +0530

    my commit

---
+++ client/src/components/common/Footer.tsx
@@ -0,0 +1,17 @@
+function Footer() {
+    return (
+        <footer className="static bottom-1 left-0 flex w-full justify-center sm:fixed">
+            <span>
+                Build with ❤️ by{" "}
+                <a
+                    href="https://github.com/02Muskan"
+                    className="text-violet-500 underline underline-offset-1"
+                >
+                  Muskan Agarwal
+                </a>
+            </span>
+        </footer>
+    )
+}
+
+export default Footer
